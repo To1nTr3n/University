9b05f35a5ea4ff25c3ab0aa9df8b760f0ae1f602
To4nTr4n <toantran12.gl@gmail.com>
2021-11-02 09:02:27 +0700

    Calculate the area of a circle

---
+++ semester7/python_advanced/lab6/s_circle.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+# from math import pi
+
+window = tk.Tk()
+def s_circle(r,pi):
+    r= r.get()
+    pi= pi.get()
+    s.set(pi * r* r)
+frame = tk.Frame(window)
+frame.pack()
+
+s= tk.DoubleVar()
+r= tk.DoubleVar()
+pi= tk.DoubleVar()
+
+tk.Label(frame, text= 'Calculate the area of a circle').pack()
+
+tk.Label(frame, text= 'Input radius').pack()
+tk.Entry(frame, textvar = r).pack()
+tk.Label(frame, text= 'input pi').pack()
+tk.Entry(frame, textvar = pi).pack()
+tk.Label(frame, text= 'The area of the circle is').pack()
+tk.Label(frame, textvar = s).pack()
+
+tk.Button(frame, text= 'Calculate', command= lambda: s_circle(r,pi)).pack()
+tk.Button(frame, text= 'Quit', command= lambda: window.destroy()).pack()
+window.mainloop()
+
+
